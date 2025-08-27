193bde4df08337a781e60976488a2e328e6b6ba8
KTrordu <alptug.yilmaz@outlook.com>
2025-08-28 02:30:47 +0300

    added collision detection to player

--- Kitchen Chaos/Assets/Scripts/Player.cs
+++ Kitchen Chaos/Assets/Scripts/Player.cs
@@ -12,7 +12,52 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * Time.deltaTime * moveSpeed; // Tüm fps'lerde ayný sayýda olmasý için böyle
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, 
+            playerRadius, moveDir, moveDistance);
+
+        if(!canMove)
+        {
+            // Cannot move towards moveDir
+
+            // Attempt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+            playerRadius, moveDirX, moveDistance);
+
+            if(canMove)
+            {
+                // Can move only on X
+                moveDir = moveDirX;
+            }
+            else
+            {
+                // Cannot move only on X
+
+                // Attempt only Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+                playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    // Can move only on Z
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    // Cannot move in ant direction
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDir * Time.deltaTime * moveSpeed; // Tüm fps'lerde ayný sayýda olmasý için böyle
+        }
 
         isWalking = moveDir != Vector3.zero;
 
