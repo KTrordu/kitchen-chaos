8258f163fbac2d91112ca9a8487fc75e6d422c19
KTrordu <alptug.yilmaz@outlook.com>
2025-08-29 03:06:08 +0300

    added picking and dropping kitchen objects to clear counters and added new container counters

--- Kitchen Chaos/Assets/Scripts/ContainerCounter.cs
+++ Kitchen Chaos/Assets/Scripts/ContainerCounter.cs
@@ -9,7 +9,7 @@ public class ContainerCounter : BaseCounter
 
     public override void Interact(Player player)
     {
-        if (!HasKitchenObject())
+        if (!player.HasKitchenObject())
         {
             Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
             kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
