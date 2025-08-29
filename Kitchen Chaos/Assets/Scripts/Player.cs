5e09aff6884698352bad98b28d6853e2ded0b5b1
KTrordu <alptug.yilmaz@outlook.com>
2025-08-29 03:18:54 +0300

    fixed movement visual bug

--- Kitchen Chaos/Assets/Scripts/Player.cs
+++ Kitchen Chaos/Assets/Scripts/Player.cs
@@ -103,7 +103,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
             // Attempt only X movement
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
-            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+            canMove = moveDir.x != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
             playerRadius, moveDirX, moveDistance);
 
             if (canMove)
@@ -117,7 +117,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
                 // Attempt only Z movement
                 Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
-                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+                canMove = moveDir.z != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
                 playerRadius, moveDirZ, moveDistance);
 
                 if (canMove)
@@ -127,7 +127,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
                 }
                 else
                 {
-                    // Cannot move in ant direction
+                    // Cannot move in any direction
                 }
             }
         }
