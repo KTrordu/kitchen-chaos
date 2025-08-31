8bc978a7546c827c7283fdf9895b098249bbb677
KTrordu <alptug.yilmaz@outlook.com>
2025-08-31 18:40:30 +0300

    added a visual UI for recipes

--- Kitchen Chaos/Assets/Scripts/DeliveryManager.cs
+++ Kitchen Chaos/Assets/Scripts/DeliveryManager.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour
 {
     public static DeliveryManager Instance { get; private set; }
+
+    public event EventHandler OnRecipeSpawned;
+    public event EventHandler OnRecipeCompleted;
 
     [SerializeField] private RecipeListSO recipeListSO;
 
@@ -27,9 +31,10 @@ public class DeliveryManager : MonoBehaviour
 
             if(waitingRecipeSOList.Count < waitingRecipesMax)
             {
-                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count)];
-                Debug.Log(waitingRecipeSO.recipeName);
+                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[UnityEngine.Random.Range(0, recipeListSO.recipeSOList.Count)];
                 waitingRecipeSOList.Add(waitingRecipeSO);
+
+                OnRecipeSpawned?.Invoke(this, EventArgs.Empty);
             }
         }
     }
@@ -68,14 +73,20 @@ public class DeliveryManager : MonoBehaviour
                 if(plateContentMatchesRecipe)
                 {
                     // Player delivered the correct Recipe
-                    Debug.Log("Player delivered the correct Recipe");
                     waitingRecipeSOList.RemoveAt(i);
+
+                    OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
+
                     return;
                 }
             }
         }
         // No matches found
         // Player did not deliver a correct Recipe
-        Debug.Log("Player did not deliver a correct Recipe");
+    }
+
+    public List<RecipeSO> GetWaitingRecipeSOList()
+    {
+        return waitingRecipeSOList;
     }
 }
