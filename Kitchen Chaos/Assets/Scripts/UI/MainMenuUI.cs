3ccc844b8be66b15b799f8e70acacfb8d7921fbb
KTrordu <alptug.yilmaz@outlook.com>
2025-09-01 19:09:13 +0300

    added pause menu

--- Kitchen Chaos/Assets/Scripts/UI/MainMenuUI.cs
+++ Kitchen Chaos/Assets/Scripts/UI/MainMenuUI.cs
@@ -13,6 +13,8 @@ public class MainMenuUI : MonoBehaviour
     {
         playButton.onClick.AddListener(PlayClick);
         quitButton.onClick.AddListener(QuitClick);
+
+        Time.timeScale = 1f;
     }
 
     private void PlayClick()
