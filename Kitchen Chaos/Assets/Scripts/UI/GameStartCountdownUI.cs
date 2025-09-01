11b93a3f2a71d36ea8dc99a5ae4c797c0bfee440
KTrordu <alptug.yilmaz@outlook.com>
2025-09-02 00:34:10 +0300

    polished the game for finishing

--- Kitchen Chaos/Assets/Scripts/UI/GameStartCountdownUI.cs
+++ Kitchen Chaos/Assets/Scripts/UI/GameStartCountdownUI.cs
@@ -5,6 +5,16 @@ public class GameStartCountdownUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI countdownText;
 
+    private const string NUMBER_POPUP = "NumberPopup";
+
+    private Animator animator;
+    private int previousCountdownNumber;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private void Start()
     {
         GameManager.Instance.OnStateChanged += GameManager_OnStateChanged;
@@ -26,7 +36,16 @@ public class GameStartCountdownUI : MonoBehaviour
 
     private void Update()
     {
+        int countdownNumber = Mathf.CeilToInt(GameManager.Instance.GetCountdownToStartTimer());
         countdownText.text = Mathf.Ceil(GameManager.Instance.GetCountdownToStartTimer()).ToString();
+
+        if(previousCountdownNumber != countdownNumber)
+        {
+            previousCountdownNumber = countdownNumber;
+            animator.SetTrigger(NUMBER_POPUP);
+
+            SoundManager.Instance.PlayCountdownSound();
+        }
     }
 
     private void Show()
